4fce3ec30ad1cadc832629e4eab34a41caab4eed
Nguyen Trien <trienvaj@gmail.com>
2021-09-23 18:11:30 +0700

    click to get data from calendar

--- chatbot-Dlu/components/CalendarComponent.js
+++ chatbot-Dlu/components/CalendarComponent.js
@@ -9,7 +9,7 @@ const CalendarComponent = (props) =>{
     LocaleConfig.locales['fr'] = {
   monthNames: ['Tháng một','tháng hai','tháng ba','tháng tư','tháng năm','tháng sáu','tháng bảy','tháng tám','tháng chín','tháng mười','tháng mười một','tháng mười hai'],
   monthNamesShort: ['Th1','Th2','Th3','Th4','Th5','TH6','Th7','Th8','Th9','Th10','Th11','Th12'],
-  dayNames: ['Thứ hai','thứ ba','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+  dayNames: ['Thứ hai','thứ ba','thứ 4','thứ năm','thứ sáu','thứ bảy','chủ nhật'],
   dayNamesShort: ['T.2','T.3','T.4.','T.5.','T.6','T.7.','CN.'],
   today: 'hôm nay\'hn'
 };
@@ -18,11 +18,48 @@ const [isModalVisible,setModalVisible] = useState(false);
 const toggleModal = () =>{
     setModalVisible(!isModalVisible);
 }
+Date.prototype.getWeek = function (dowOffset) {
+/*getWeek() was developed by Nick Baicoianu at MeanFreePath: http://www.meanfreepath.com */
+
+    dowOffset = typeof(dowOffset) == 'number' ? dowOffset : 0; //default dowOffset to zero
+    var newYear = new Date(this.getFullYear(),0,1);
+    var day = newYear.getDay() - dowOffset; //the day of week the year begins on
+    day = (day >= 0 ? day : day + 7);
+    var daynum = Math.floor((this.getTime() - newYear.getTime() - 
+    (this.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
+    var weeknum;
+    //if the year starts before the middle of a week
+    if(day < 4) {
+        weeknum = Math.floor((daynum+day-1)/7) + 1;
+        if(weeknum > 52) {
+            nYear = new Date(this.getFullYear() + 1,0,1);
+            nday = nYear.getDay() - dowOffset;
+            nday = nday >= 0 ? nday : nday + 7;
+            /*if the next year starts before the middle of
+              the week, it is week #1 of that year*/
+            weeknum = nday < 4 ? 1 : 53;
+        }
+    }
+    else {
+        weeknum = Math.floor((daynum+day-1)/7);
+    }
+    return weeknum;
+};
+
+const getDate = (date) =>{
+  //{"dateString": "2021-09-22", "day": 22, "month": 9, "timestamp": 1632268800000, "year": 2021}
+  const message={dayName:'',week:'',year:''}
+  message.week = new Date(date.year,date.month-1,date.day).getWeek();
+  message.year = date.year;
+  const arrDayNames = ['chủ nhật','Thứ hai','thứ ba','thứ 4','thứ năm','thứ sáu','thứ bảy']
+  const d = new Date(date.dateString);
+  message.dayName = arrDayNames[d.getDay()];
+  console.log(message);
+
+}
     return(
-        <View style={styles.container}>
-        <TouchableOpacity >
+        <TouchableOpacity style={styles.container}>   
             <Agenda  name='calendar' color="white" size={30} onPress={toggleModal}/> 
-        </TouchableOpacity>
           <Modal animationType="fade" 
           transparent={true} 
           visible={isModalVisible}
@@ -30,11 +67,19 @@ const toggleModal = () =>{
              >
                  <TouchableOpacity style={styles.modalContainer} onPress={() => { setModalVisible(false)}}>
                         <TouchableOpacity style={styles.modal}  activeOpacity={1} >
-                            <Calendar style={styles.calendarView}/>
+                            <Calendar
+                            onDayPress={(day) =>{getDate(day)}}
+                             style={styles.calendarView} 
+                              pagingEnabled={true} 
+                              horizontal={true} 
+                              calendarWidth={320}
+                              theme={{
+                                calendarBackground: '#ffffff',
+                              }}/>
                         </TouchableOpacity>
                     </TouchableOpacity>
             </Modal>
-        </View>
+       </TouchableOpacity>
     );
 }
 function mapStateToProps(state) {
@@ -57,6 +102,9 @@ const styles = StyleSheet.create({
     },
     calendarView:{
         justifyContent:'center',
+        height:400,
+        borderRadius:30,
+
     },
      modalContainer: {
          flex: 1,
@@ -71,6 +119,6 @@ const styles = StyleSheet.create({
   },
   modal:{
         width:'90%' ,
-      height: 300,
+      height: 400,
   },  
 })
